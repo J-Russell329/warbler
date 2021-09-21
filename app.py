29a8af869c8aec3ada9cd7d4a06a731596178e76
root <jacob@JacobPC.localdomain>
2021-09-20 20:57:23 -0500

    added in a proc file and changed the db location

--- app.py
+++ app.py
@@ -13,8 +13,13 @@ app = Flask(__name__)
 
 # Get DB_URI from environ variable (useful for production/testing) or,
 # if not set there, use development local db.
-app.config['SQLALCHEMY_DATABASE_URI'] = (
-    os.environ.get('DATABASE_URL', 'postgres:///warbler'))
+
+
+uri = os.environ.get("DATABASE_URL", 'postgresql:///warbler')  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+if uri == "postgresql:///warbler":
+    app.config['SQLALCHEMY_DATABASE_URI'] = uri
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
